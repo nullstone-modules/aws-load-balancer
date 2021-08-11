2d60c81572433cab49850e26360bc77b3b0cd657
Brad Sickles <brad.sickles@gmail.com>
2021-08-11 08:24:22 -0400

    Fix lb_url

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ locals {
   protocol         = var.enable_https ? lower(aws_lb_listener.https.protocol) : lower(aws_lb_listener.http.protocol)
   port             = var.enable_https ? aws_lb_listener.https.port : aws_lb_listener.http.port
   lb_subdomain     = [aws_lb.this.dns_name]
-  lb_url           = "${local.protocol}://${local.lb_url}:${local.port}"
+  lb_url           = "${local.protocol}://${local.lb_subdomain}:${local.port}"
   vanity_subdomain = try(trimsuffix(aws_route53_record.alias[0].fqdn, "."), "")
   vanity_url       = "${local.protocol}://${local.vanity_subdomain}:${local.port}"
 }
