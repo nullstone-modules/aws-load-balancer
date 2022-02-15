ca1394beea978203d490faa1036ef1529f1acc72
Brad Sickles <brad.sickles@gmail.com>
2022-02-15 10:43:01 -0500

    Break out lifecycle_configuration, versioning, and server side encrypt

--- logs_bucket/main.tf
+++ logs_bucket/main.tf
@@ -3,16 +3,27 @@ resource "aws_s3_bucket" "default" {
   acl           = "log-delivery-write"
   force_destroy = var.force_destroy
   policy        = data.aws_iam_policy_document.default.json
+  tags          = merge({ Name : var.name }, var.tags)
+}
 
-  tags = merge({ Name : var.name }, var.tags)
+resource "aws_s3_bucket_versioning" "default" {
+  bucket = aws_s3_bucket.default.id
 
-  versioning {
-    enabled = true
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  lifecycle_rule {
+resource "aws_s3_bucket_lifecycle_configuration" "default" {
+  bucket = aws_s3_bucket.default.bucket
+
+  rule {
     enabled                                = true
     abort_incomplete_multipart_upload_days = 5
+
+    expiration {
+      days = 90
+    }
 
     noncurrent_version_expiration {
       days = 90
@@ -32,17 +43,15 @@ resource "aws_s3_bucket" "default" {
       days          = 60
       storage_class = "GLACIER"
     }
-
-    expiration {
-      days = 90
-    }
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.default.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
